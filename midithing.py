a931f114d6600f30480bed46cea8c94780609778
Richard Whalley <rich.whalley@gmail.com>
2021-05-16 23:28:58 -0400

    initial commit

---
+++ midithing.py
@@ -0,0 +1,91 @@
+import rtmidi
+from soundy_pygame import Soundy
+from pynput import keyboard
+from os import listdir
+from os.path import isfile, join
+from threading import Thread
+
+
+class MidiControl:
+
+    def __init__(self):
+
+        self.basepath = './samples/'
+        self.current_bank = 1
+        self.max_sample_length_seconds = 1
+        self.max_bank_size = 16
+        self.load_samples()
+
+        midiin = rtmidi.RtMidiIn()
+        ports = range(midiin.getPortCount())
+        if ports:
+            for i in ports:
+                print(midiin.getPortName(i))
+            print("Opening port 0!")
+            midiin.openPort(0)
+            while True:
+                m = midiin.getMessage(10) # some timeout in ms
+                if m:
+                    self.print_message(m)
+        else:
+            print('NO MIDI INPUT PORTS!')
+
+    def load_samples(self):
+        path = self.basepath + str(self.current_bank)+'/'
+        onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+        self.sounds = []
+        for file in onlyfiles:
+            if file.endswith('.wav'):
+                if file.startswith('.'):
+                    pass
+                else:
+                    self.sounds.append(Soundy(path+file))
+        self.sounds = self.sounds[0:self.max_bank_size]
+        for sound in self.sounds:
+            sound.restrict_length(self.max_sample_length_seconds)  # Truncate Samples longer than n seconds
+            sound.remove_artifacts()
+
+
+    def print_message(self,midi):
+        try:
+            #print('ON: ', midi.getMidiNoteName(midi.getNoteNumber()), midi.getVelocity())
+            note = midi.getNoteNumber()
+            if midi.isNoteOn():
+
+                if note != 22 and note !=23:
+                    i = note-36
+                    #self.sounds[i].stop()
+                    #self.sounds[i].set_volume(midi.getVelocity())
+                    self.sounds[i].play(block=False)
+
+                else:
+                    if note == 22:
+                        self.current_bank += 1
+                        try:
+                            self.load_samples()
+                        except FileNotFoundError:
+                            self.current_bank -= 1
+                    elif note == 23:
+                        self.current_bank -= 1
+                        try:
+                            self.load_samples()
+                        except FileNotFoundError:
+                            self.current_bank += 1
+            elif midi.isNoteOff():
+                #print('OFF:', midi.getMidiNoteName(midi.getNoteNumber()))
+                i = midi.getNoteNumber()-36
+                self.sounds[i].stop()
+            elif midi.isController():
+                if(midi.getNoteNumber() == 10):
+                    for sound in self.sounds:
+                        factor = 1.5 - midi.getControllerValue()/128.
+                        print(factor)
+                        x = Thread(sound.change_pitch(factor))
+                        x.start()
+
+                #print('CONTROLLER', midi.getControllerNumber(), midi.getControllerValue())
+        except IndexError:
+            pass
+
+
+MidiControl()
---
+++ soundy_pygame.py
@@ -0,0 +1,81 @@
+import pygame as pg
+from time       import sleep
+import numpy as np
+from samplerate import resample
+from scipy import signal
+from threading import Thread
+#import resampy
+
+class Soundy:
+
+    def __init__(self,soundpath):
+        self.sample_rate = 44100
+        pg.mixer.init(frequency=self.sample_rate, size=-16, channels=2, buffer=32)
+        pg.init()
+        self.pgsound = pg.mixer.Sound(soundpath)
+        self.original_sound = pg.mixer.Sound(soundpath)
+
+    def restrict_length(self,len_in_seconds):
+        self.pgsound = pg.sndarray.make_sound(pg.sndarray.array(self.pgsound)[:int(self.sample_rate*len_in_seconds),:])
+
+
+
+    def remove_artifacts(self):
+
+        snd_array = pg.sndarray.array(self.pgsound)
+
+        # --- REMOVE SILENCE AT BEGINNING OF SAMPLES ---
+        sound_start = None
+        for i, sample in enumerate(snd_array):
+            if abs(sample.sum()) > 150:
+                sound_start = i
+                break
+        if sound_start:
+            snd_array = snd_array[sound_start:,:]
+
+        # --- FADE BEGINNING AND END OF SAMPLE TO REMOVE HIGH FREQUENCY ARTIFACTS ---
+        #fade = 200. # Fade in MS
+        #fade_in = np.arange(0., 1., 1/fade)
+        #fade_in = fade_in*fade_in
+        #fade_out = np.arange(1., 0., -1/fade)
+        #fade = int(fade)
+        #for i in range (0,2):
+        #    snd_array[:fade,i] = np.multiply(snd_array[:fade,i], fade_in)
+        #    snd_array[-fade:,i] = np.multiply(snd_array[-fade:,i], fade_out)
+
+        # --- SET NEW DEFAULT SOUND AFTER PROCESSING ---
+        self.pgsound = pg.sndarray.make_sound(snd_array)
+        self.original_sound=self.pgsound
+
+
+    def lowpass_filter(self):
+        N=5
+        Wn=0.1
+        B,A = signal.butter(N,Wn,output='ba')
+        snd_array = pg.sndarray.array(self.pgsound)
+        for i in range (0,2):
+            snd_array[:,i] = signal.filtfilt(B,A,snd_array[:,i])
+        self.pgsound = pg.sndarray.make_sound(snd_array)
+
+    def change_pitch(self,factor):
+        snd_array = pg.sndarray.array(self.original_sound)
+        #factor = int(len(snd_array)*factor)
+        #snd_resample = resampy.resample(snd_array,self.sample_rate, int(self.sample_rate*factor) - int(self.sample_rate*factor)%128 ,axis=0)
+        #snd_resample = signal.resample(snd_array,factor).astype(snd_array.dtype)
+        snd_resample=resample(snd_array,factor,'sinc_fastest').astype(snd_array.dtype)
+        self.pgsound = pg.sndarray.make_sound(snd_resample)
+
+    def set_volume(self,midi_vel_in):
+        normalized_vel = midi_vel_in/128.
+        self.pgsound.set_volume(normalized_vel)
+
+    def play(self, block = True):
+
+        self.pgsound.play()
+
+        if block:
+            sleep(self.nssound.duration())
+
+    def stop(self):
+        #self.pgsound.fadeout(200)
+        self.pgsound.stop()
