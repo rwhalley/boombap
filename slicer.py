9f78b4dc3e59f103cdd1751d08d03925f163039b
Richard Whalley <rich.whalley@gmail.com>
2021-05-17 01:08:05 -0400

    add basic slicer

---
+++ slicer.py
@@ -0,0 +1,22 @@
+import librosa
+import numpy as np
+import soundfile as sf
+
+class Slicer:
+    def __init__(self,input_wav_file):
+        self.input_wav, self.sample_rate = librosa.load(input_wav_file)
+        print(self.input_wav)
+        self.max_onsets = 16
+        self.onsets = librosa.onset.onset_detect(y=self.input_wav, sr=self.sample_rate, units='samples')
+        self.sliced =[ ]
+        for i,onset in enumerate(self.onsets):
+            if i==16:
+                break
+            else:
+                print(onset)
+                data =(self.input_wav[onset:self.onsets[i+1]-2000])
+                sf.write('./samples/4/test'+str(i)+'.wav', data, self.sample_rate, subtype='PCM_24')
+
+
+
+Slicer("./samples/2/  - Marker #19.wav")
