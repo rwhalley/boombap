9607cbb3e14253ee57962093a043045c7c872373
Richard Whalley <rich.whalley@gmail.com>
2022-01-08 17:13:10 -0500

    make rhythm dictionary

--- rhythm.py
+++ rhythm.py
@@ -1,14 +1,30 @@
+from drum import Drum
+
 class Rhythm:
 
     def __init__(self):
         self.name = []
-        self.mbalax1 = []
-        self.mbalax2 = []
-        self.talmbat = []
-        self.tulli = []
-        self.tugone1 = []
-        self.tugone2 = []
-        self.nder = []
+        self.drums = {
+            "mbalax1": Drum("mbalax1"),
+            "mbalax2": Drum("mbalax1"),
+            "talmbat": Drum("talmbat"),
+            "tulli": Drum("Tulli"),
+            "tugone1": Drum("Tugone"),
+            "tugone2": Drum("Tugone"),
+            "kung1": Drum("Kung"),
+            "kung2": Drum("Kung"),
+            "nder": Drum("Nder")
+
+        }
+        self.mbalax1 = Drum("Mbung Mbung")
+        self.mbalax2 = Drum("Mbung Mbung")
+        self.talmbat = Drum("talmbat")
+        self.tulli = Drum("Tulli")
+        self.tugone1 = Drum("Tugone")
+        self.tugone2 = Drum("Tugone")
+        self.kung1 = Drum("Kung")
+        self.kung2 = Drum("Kung")
+        self.nder = Drum("N'Der")
         self.metronome = []
         self.parts = []
         self.notes_per_beat = None
