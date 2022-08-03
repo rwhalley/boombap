de49ab8161eea430f77595f143d39b12a83d51d4
Richard Whalley <rich.whalley@gmail.com>
2022-08-03 14:05:48 -0400

    try_midiout for pi LED out

--- try_midiout.py
+++ try_midiout.py
@@ -1,18 +1,42 @@
 import mido
 import time
+import CONFIG as c
+import note_class
 
-options = list(set(mido.get_input_names()))
-print(options)
+outport = None
+light = True
+if light:
+    dev = c.MIDI_CONTROLLER
+    devs = mido.get_output_names()
+    print(devs)
+    for d in devs:
+        print(dev)
+        print(d)
+        if dev in d:
+            print("Opening")
+            outport = mido.open_output(d)
 
-msg = mido.Message('note_on', note=44, velocity=100)
-msg_off = mido.Message('note_off', note=44, velocity=0)
-outport = mido.open_output('reface CP')
-outport.send(msg)
-outport.close()
-time.sleep(1)
-outport = mido.open_output('reface CP')
-outport.panic()
-outport.close()
+for i in range(0,60):
+    outport.send(note_class.Note(0,mido.Message('note_on', note=i),0,c.MIDI_CONTROLLER,time.time(), -2,0), light=True)
+    time.sleep(0.005)
+
+for i in range(0,60):
+    outport.send(note_class.Note(0,mido.Message('note_off', note=i),0,c.MIDI_CONTROLLER,time.time(), -2,0), light=True)
+    time.sleep(0.005)
+
+
+# options = list(set(mido.get_input_names()))
+# print(options)
+#
+# msg = mido.Message('note_on', note=44, velocity=100)
+# msg_off = mido.Message('note_off', note=44, velocity=0)
+# outport = mido.open_output('reface CP')
+# outport.send(msg)
+# outport.close()
+# time.sleep(1)
+# outport = mido.open_output('reface CP')
+# outport.panic()
+# outport.close()
 
 
 # with mido.open_output('reface CP') as outport:
