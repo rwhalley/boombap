ab1ce6542a3ae44fac05fa6a82cbc349904b15c2
Richard Whalley <rich.whalley@gmail.com>
2023-01-02 11:19:38 -0500

    update scripts

--- scripts/resize.py
+++ scripts/resize.py
@@ -6,9 +6,9 @@ import sys
 sys.path.append('../')
 import CONFIG as c
 
-vi= c.VIDEO_INPUT_PATH + "IMG_1442.MOV"
+vi= c.VIDEO_INPUT_PATH + "IMG_1466.MOV"
 #vi = "../piggo.mp4" #video input filename
-vo = c.VIDEO_INPUT_PATH + "vladsaw.mp4"
+vo = c.VIDEO_INPUT_PATH + "catsit.mp4"
 #vo = "pigggo.mp4" # video output filename
 width = 480 # keep aspect ratio
 
